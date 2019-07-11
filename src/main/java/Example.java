8ec139680cd1867508b8c1b37cf0a35b957da165
mmedellin2786 <52011931+mmedellin2786@users.noreply.github.com>
2019-07-11 12:11:49 -0500

    Update Example.java

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,12 +1,19 @@
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
 @RestController
 @EnableAutoConfiguration
 public class Example {
-        @RequestMapping("/")
-        String home() {
-                return "Hello World!";
+        @RequestMapping(method=RequestMethod.POST, value="/")
+        String newRecurso(@RequestParam String name, @RequestParam String skills) {
+                            Recurso recurso = new Recurso(name, skills);
+                            System.out.println(recurso.getName());
+                List<Recurso> listRec = new ArrayList<Recurso>();
+                listRec.add(recurso);
+                                return "Inserted..." + recurso.getName();
+
         }
         public static void main(String[] args) throws Exception {
                 SpringApplication.run(Example.class, args);
