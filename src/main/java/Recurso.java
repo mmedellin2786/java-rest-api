27648b3de0187724718210ccb67e6ec16fd04cfb
mmedellin2786 <52011931+mmedellin2786@users.noreply.github.com>
2019-07-11 12:11:07 -0500

    Create Recurso.java

---
+++ src/main/java/Recurso.java
@@ -0,0 +1,16 @@
+public class Recurso {
+
+  private String name;
+  private String skills;
+
+  Recurso() {}
+
+  public Recurso(String name, String skills) {
+    this.name = name;
+    this.skills = skills;
+  }
+
+  public String getName() {
+     return name;
+  }
+}
